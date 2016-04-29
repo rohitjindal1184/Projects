944c77299fc33d52296bf0218cbc0f381f69423a
Rohit <mobileprogramming@Rohits-MacBook-Pro.local>
2016-04-29 01:47:15 -0400

    update

---
+++ Lokay_iOS8 2/anywall/DropBoxFileView.h
@@ -0,0 +1,13 @@
+//
+//  DropBoxFileView.h
+//  lokay
+//
+//  Created by Mobile Programming on 4/28/16.
+//  Copyright © 2016 Parse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DropBoxFileView : UIView
+
+@end
---
+++ Lokay_iOS8 2/anywall/DropBoxFileView.m
@@ -0,0 +1,21 @@
+//
+//  DropBoxFileView.m
+//  lokay
+//
+//  Created by Mobile Programming on 4/28/16.
+//  Copyright © 2016 Parse. All rights reserved.
+//
+
+#import "DropBoxFileView.h"
+
+@implementation DropBoxFileView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
